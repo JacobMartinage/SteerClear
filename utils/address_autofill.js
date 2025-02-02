3542004f8baade809e81bf276dc0ae36e1ccfd6f
owenstuckman <ostuckman@outlook.com>
2025-02-01 22:53:52 -0500

    prompt changes

---
+++ utils/address_autofill.js
@@ -0,0 +1,13 @@
+import Mapbox from '@rnmapbox/maps';
+
+
+//set token
+Mapbox.setAccessToken('sk.eyJ1IjoiamFxdWliaXMiLCJhIjoiY202bWp6Z2ZzMGtraDJrcHoxNjdrbm9qdSJ9.fix3XfnvCj6cqlj6D3vFpg');
+
+
+export async function autofill(){
+    
+}
+
+
+
