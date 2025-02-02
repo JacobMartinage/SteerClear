119ced605c9b9054e5d93534244955f42f9f78a1
owenstuckman <ostuckman@outlook.com>
2025-02-02 04:43:36 -0500

    finished college info

--- utils/college_info.js
+++ utils/college_info.js
@@ -3,10 +3,37 @@ class CollegeInfo {
 
      // get list of existing active groups
      static async getColleges() {
+        const { data, error } = await supabase.from('colleges').select()
+        if (error) console.error('Error fetching groups:', error);
+        return data;
+    }
+
+     // get list of existing active groups
+     static async getCollegesNames() {
         const { data, error } = await supabase.from('colleges').select('name')
         if (error) console.error('Error fetching groups:', error);
         return data;
     }
+
+     // get list of existing active groups
+     static async addCollege(collegename) {
+        username = await getUsername;
+        const { data, error } = await supabase.from('profiles').update([{college: collegename}]).eq('username', username)
+        if (error) console.error('Error fetching groups:', error);
+        return data;
+    }
+
+     // Parse resources from JSON to make them easy to use in components
+     static parseCollegeResources(resourcesJson) {
+        return resourcesJson.resources.map(resource => ({
+            name: resource.name,
+            description: resource.description,
+            contact: resource.contact,
+            services: resource.services
+        }));
+    }
+
+
 
 
 }
